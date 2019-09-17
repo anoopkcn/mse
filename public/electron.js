8c961541c0d84c94316e46bd96164096835c42cb
Anoop Chandran <anoopkcn@gmail.com>
2019-09-17 14:16:04 +0200

    Initial commit

---
+++ public/electron.js
@@ -0,0 +1,67 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const ipcMain = electron.ipcMain;
+
+const glob = require('glob');
+const path = require('path');
+const url = require('url');
+const isDev = require('electron-is-dev');
+
+const { channels } = require(isDev ? '../src/shared/constants' : './src/shared/constants' );
+
+let mainWindow = null
+
+function initialize () {
+    makeSingleInstance()
+    
+    function createWindow() {
+      mainWindow = new BrowserWindow({
+        width: 900, 
+        height: 680,
+        webPreferences: {
+          nodeIntegration: true,
+          preload: path.join(__dirname, 'preload.js'),        }
+      });
+      mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+      mainWindow.on('closed', () => mainWindow = null);
+    }
+
+    app.on('ready', createWindow);
+
+    app.on('window-all-closed', () => {
+      if (process.platform !== 'darwin') {
+        app.quit();
+      } 
+    });
+
+    app.on('activate', () => {
+      if (mainWindow === null) {
+        createWindow();
+      }
+    });
+
+    ipcMain.on(channels.APP_INFO, (event) => {
+      event.sender.send(channels.APP_INFO, { 
+        appName: app.getName(),
+        appVersion: app.getVersion(),
+      });
+    });
+}
+
+// Returns true if the current version of the app should quit instead of
+// launching.
+function makeSingleInstance () {
+  if (process.mas) return
+
+  app.requestSingleInstanceLock()
+
+  app.on('second-instance', () => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
+}
+
+initialize()
---
+++ src/App.js
@@ -0,0 +1,34 @@
+// eElectron modules are required using window.require()
+import React, {Component} from 'react';
+import './App.css';
+import { channels } from './shared/constants';
+const { ipcRenderer } = window; 
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      appName: '',
+      appVersion: '',
+    }
+    ipcRenderer.send(channels.APP_INFO);
+    ipcRenderer.on(channels.APP_INFO, (event, arg) => {
+      ipcRenderer.removeAllListeners(channels.APP_INFO);
+      const { appName, appVersion } = arg;
+      this.setState({ appName, appVersion });
+    });
+  }
+
+  render() {
+    const { appName, appVersion } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>{appName} version {appVersion}</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
