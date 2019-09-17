f3f9c4fc2afef4e8c6664345122582a48329d69d
Anoop Chandran <anoopkcn@gmail.com>
2019-09-17 18:30:14 +0200

    Getting data from the REST API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 // eElectron modules are required using window.require()
 import React, {Component} from 'react';
 import './App.css';
+
 import { channels } from './shared/constants';
+import Users from './components/users';
+import Computers from './components/computers';
+import Calculations from './components/calculations';
+
 const { ipcRenderer } = window; 
 
 class App extends Component {
@@ -18,7 +23,6 @@ class App extends Component {
       this.setState({ appName, appVersion });
     });
   }
-
   render() {
     const { appName, appVersion } = this.state;
     return (
@@ -26,7 +30,17 @@ class App extends Component {
         <header className="App-header">
           <p>{appName} version {appVersion}</p>
         </header>
+        {/*<div className="Users">
+          <Users />
+        </div>
+        <div className="Computers">
+          <Computers />
+        </div> */}
+        <div className="Calculations">
+          <Calculations />
+        </div>
       </div>
+
     );
   }
 }
---
+++ src/components/calculations.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import axios from 'axios';
+import {API_URL} from './global';
+const url = `${API_URL}/calculations`
+
+export default class Calculations extends Component {
+  state = {
+    error: null,
+    isLoaded: false,
+    items: {}
+  };
+
+  componentDidMount() {
+        this.fetchCalc();
+        // this.timer = setInterval(() => this.fetchCalc(), 1000);
+  }
+  // componentWillUnmount() {
+  //       clearInterval(this.timer);
+  //       this.timer = null;
+  // }
+  async fetchCalc() {
+        try {
+            this.setState({...this.state, isLoaded: false});
+            const result = await axios.get(url);
+            this.setState({items: result.data, isLoaded: true});
+        } catch (error) {
+            this.setState({error, isLoaded: true});
+        }
+    };
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+         <ul>
+          {items.data.calculations.map(item => (
+            <li key={item.id}>
+              {item.id} {item.attributes.function_name}  {item.attributes.process_label} {item.attributes.process_state}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+}
