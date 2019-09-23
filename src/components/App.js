28d0e6f3b79a49cd40fa95566ee9e2825ca1cc1a
Anoop Chandran <anoopkcn@gmail.com>
2019-09-24 00:47:21 +0200

    Drawer moved to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,62 @@
 // Electron modules are required using window.require()
 import React, { useEffect, useState } from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import MenuOpenIcon from "@material-ui/icons/MenuOpen";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
 
 import { channels } from "../shared/constants";
 import Dashboard from "./Dashboard";
 import { writeConfig } from "../lib/global";
+import { mainListItems } from "./listItems";
 
 const { ipcRenderer } = window;
+
+const drawerWidth = 220;
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: "flex"
+    },
+    toolbar: {
+        paddingRight: 20, // keep right padding when drawer closed
+        backgroundColor: "#115293"
+    },
+    toolbarIcon: {
+        position: "absolute",
+        bottom: theme.spacing(1),
+        right: theme.spacing(1)
+    },
+    drawerPaper: {
+        position: "relative",
+        whiteSpace: "nowrap",
+        width: drawerWidth,
+        transition: theme.transitions.create("width", {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen
+        })
+    },
+    drawerPaperClose: {
+        overflowX: "hidden",
+        transition: theme.transitions.create("width", {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up("sm")]: {
+            width: theme.spacing(8)
+        }
+    }
+}));
 
 export default function App() {
     const [appName, setAppName] = useState("");
     const [appVersion, setAppVersion] = useState("");
+
     useEffect(() => {
         ipcRenderer.send(channels.APP_INFO);
         ipcRenderer.on(channels.APP_INFO, (event, arg) => {
@@ -20,11 +67,50 @@ export default function App() {
         writeConfig();
     }, []);
 
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+    let drawerState;
+    if (open) {
+        drawerState = (
+            <IconButton onClick={handleDrawerClose}>
+                <MenuOpenIcon fontSize="small" />
+            </IconButton>
+        );
+    } else {
+        drawerState = (
+            <IconButton onClick={handleDrawerOpen}>
+                <MenuIcon fontSize="small" />
+            </IconButton>
+        );
+    }
+
     console.log(appName, appVersion);
 
     return (
         <div className="App">
-            <Dashboard />
+            <div className={classes.root}>
+                <CssBaseline />
+                <Drawer
+                    variant="permanent"
+                    classes={{
+                        paper: clsx(
+                            classes.drawerPaper,
+                            !open && classes.drawerPaperClose
+                        )
+                    }}
+                    open={open}
+                >
+                    <List>{mainListItems}</List>
+                    <div className={classes.toolbarIcon}>{drawerState}</div>
+                </Drawer>
+                <Dashboard />
+            </div>
         </div>
     );
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,73 +1,20 @@
 import React from "react";
 import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Drawer from "@material-ui/core/Drawer";
-import List from "@material-ui/core/List";
-import IconButton from "@material-ui/core/IconButton";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
-// import Link from '@material-ui/core/Link';
-import MenuIcon from "@material-ui/icons/Menu";
-import MenuOpenIcon from '@material-ui/icons/MenuOpen';
-import { mainListItems } from "./listItems";
 import Profile from "./Profile";
 import Status from "./Status";
 import Nodes from "./Nodes";
-
-const drawerWidth = 220;
 
 const useStyles = makeStyles(theme => ({
     root: {
         display: "flex"
     },
-    toolbar: {
-        paddingRight: 20, // keep right padding when drawer closed
-        backgroundColor: "#115293"
-    },
-    toolbarIcon: {
-        position: 'absolute',
-        bottom: theme.spacing(1),
-        right: theme.spacing(1),
-    },
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(["width", "margin"], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen
-        })
-    },
-    menuButton: {
-        marginRight: 36
-    },
-    menuButtonHidden: {
-        display: "none"
-    },
     title: {
         flexGrow: 1
     },
-    drawerPaper: {
-        position: "relative",
-        whiteSpace: "nowrap",
-        width: drawerWidth,
-        transition: theme.transitions.create("width", {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen
-        })
-    },
-    drawerPaperClose: {
-        overflowX: "hidden",
-        transition: theme.transitions.create("width", {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen
-        }),
-        width: theme.spacing(7),
-        [theme.breakpoints.up("sm")]: {
-            width: theme.spacing(8)
-        }
-    },
-    appBarSpacer: theme.mixins.toolbar,
     content: {
         flexGrow: 1,
         height: "100vh",
@@ -90,69 +37,29 @@ const useStyles = makeStyles(theme => ({
 
 export default function Dashboard() {
     const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
     const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
-    let drawerState;
-    if(open){
-        drawerState = (
-            <IconButton onClick={handleDrawerClose}>
-               <MenuOpenIcon fontSize="small"/>
-             </IconButton>
-            )
-    }else{
-        drawerState = (
-        <IconButton onClick={handleDrawerOpen}>
-            <MenuIcon fontSize="small"/>
-         </IconButton>
-        )
-    }
-    // onClick={handleDrawerOpen}
     return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <Drawer
-                variant="permanent"
-                classes={{
-                    paper: clsx(
-                        classes.drawerPaper,
-                        !open && classes.drawerPaperClose
-                    )
-                }}
-                open={open}
-            >
-                <List>{mainListItems}</List>
-                <div className={classes.toolbarIcon}>
-                { drawerState}
-                </div>
-            </Drawer>
-            <main className={classes.content}>
-                <Container maxWidth="lg" className={classes.container}>
-                    <Grid container spacing={3}>
-                        {/* Recent Deposits */}
-                        <Grid item xs={12} md={6} lg={6}>
-                            <Paper className={fixedHeightPaper}>
-                                <Profile />
-                            </Paper>
-                        </Grid>
-                        {/* Chart */}
-                        <Grid item xs={12} md={6} lg={6}>
-                            <Paper className={fixedHeightPaper}>
-                                <Status />
-                            </Paper>
-                        </Grid>
-                        {/* Recent Orders */}
-                        <Grid item xs={12}>
-                            <Nodes />
-                        </Grid>
+        <main className={classes.content}>
+            <Container maxWidth="lg" className={classes.container}>
+                <Grid container spacing={3}>
+                    {/* Recent Deposits */}
+                    <Grid item xs={12} md={6} lg={6}>
+                        <Paper className={fixedHeightPaper}>
+                            <Profile />
+                        </Paper>
                     </Grid>
-                </Container>
-            </main>
-        </div>
+                    {/* Chart */}
+                    <Grid item xs={12} md={6} lg={6}>
+                        <Paper className={fixedHeightPaper}>
+                            <Status />
+                        </Paper>
+                    </Grid>
+                    {/* Recent Orders */}
+                    <Grid item xs={12}>
+                        <Nodes />
+                    </Grid>
+                </Grid>
+            </Container>
+        </main>
     );
 }
