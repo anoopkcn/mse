df331a5aa3d8da3aa9ce832556d758fdd737d0af
Anoop Chandran <anoopkcn@gmail.com>
2019-09-24 00:34:11 +0200

    Convert Class to Function using react hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 // Electron modules are required using window.require()
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 
 import { channels } from "../shared/constants";
 import Dashboard from "./Dashboard";
@@ -7,30 +7,24 @@ import { writeConfig } from "../lib/global";
 
 const { ipcRenderer } = window;
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            appName: "",
-            appVersion: ""
-        };
+export default function App() {
+    const [appName, setAppName] = useState("");
+    const [appVersion, setAppVersion] = useState("");
+    useEffect(() => {
         ipcRenderer.send(channels.APP_INFO);
         ipcRenderer.on(channels.APP_INFO, (event, arg) => {
             ipcRenderer.removeAllListeners(channels.APP_INFO);
-            const { appName, appVersion } = arg;
-            this.setState({ appName, appVersion });
+            setAppName(arg.appName);
+            setAppVersion(arg.appVersion);
         });
         writeConfig();
-    }
-    render() {
-        const { appName, appVersion } = this.state;
-        console.log(appName, appVersion);
-        return (
-            <div className="App">
-                <Dashboard />
-            </div>
-        );
-    }
-}
+    }, []);
 
-export default App;
+    console.log(appName, appVersion);
+
+    return (
+        <div className="App">
+            <Dashboard />
+        </div>
+    );
+}
