3fb85a13eb01dd915396ce4785864db5f4864095
Anoop Chandran <anoopkcn@gmail.com>
2019-09-23 14:22:39 +0200

    Turn class into a function using react hooks

--- src/components/Nodes.js
+++ src/components/Nodes.js
@@ -1,47 +1,38 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Paper from "@material-ui/core/Paper";
 import NodesTable from "./NodesTable";
 import { AIIDA_RESTAPI_URL, startServer } from "../lib/global";
 
 const url = `${AIIDA_RESTAPI_URL}/nodes?orderby=-id`;
 
-export default class Nodes extends Component {
-    state = {
-        error: null,
-        isLoaded: false,
-        items: {}
-    };
+export default function Nodes() {
+    const [data, setData] = useState({})
+    const [isLoaded, setLoaded]=useState(false)
 
-    componentDidMount() {
-        startServer();
-        this.fetchCalc();
-        this.timer = setInterval(() => this.fetchCalc(), 3000);
-    }
-    componentWillUnmount() {
-        clearInterval(this.timer);
-        this.timer = null;
-    }
-    fetchCalc() {
+    startServer();
+    useEffect(() => {
+        fetchCalc();
+        setInterval(() => fetchCalc(), 3000);
+    });
+
+    function fetchCalc() {
         if (AIIDA_RESTAPI_URL) {
             fetch(url)
                 .then(result => result.json())
                 .then(result => {
-                    this.setState({ items: result, isLoaded: true });
+                    setData(result);
                 })
                 .catch(error => {
-                    this.setState({ error, isLoaded: true });
+                    setLoaded(isLoaded);
                 });
         }
     }
-
-    render() {
-        var items = this.state.items;
+        // var data = state.data;
         return (
             <React.Fragment>
-            <Paper style={{ height: 500, width: "100%" }}>
-                {items && items.data && <NodesTable data={items} />}
+            <Paper style={{ minHeight: 300, width: "100%" }}>
+                {data && data.data && <NodesTable data={data} />}
             </Paper>
             </React.Fragment>
         );
-    }
 }
