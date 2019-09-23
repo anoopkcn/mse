f56a5d2167d6a25b43e6092d23d8789f68f97256
Anoop Chandran <anoopkcn@gmail.com>
2019-09-23 15:22:38 +0200

    move code blocks to attain best performance

--- src/components/Nodes.js
+++ src/components/Nodes.js
@@ -1,38 +1,60 @@
 import React, { useEffect, useState } from "react";
 import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
 import NodesTable from "./NodesTable";
 import { AIIDA_RESTAPI_URL, startServer } from "../lib/global";
 
 const url = `${AIIDA_RESTAPI_URL}/nodes?orderby=-id`;
 
+const useStyles = makeStyles(theme => ({
+    root: {
+         minHeight: 300, 
+         width: "100%",
+         // boxShadow: '0 0 0 0',
+    },
+}));
+
+function fetchCalc() {
+    if (AIIDA_RESTAPI_URL) {
+        return fetch(url)
+            .then(result => result.json())
+            .then(result => {
+                return result;
+            })
+            .catch(error => {
+                return error;
+            });
+    }
+}
+
 export default function Nodes() {
-    const [data, setData] = useState({})
-    const [isLoaded, setLoaded]=useState(false)
+    const [data, setData] = useState({});
+    const [isLoaded, setLoaded] = useState(false);
 
-    startServer();
     useEffect(() => {
-        fetchCalc();
-        setInterval(() => fetchCalc(), 3000);
-    });
-
-    function fetchCalc() {
-        if (AIIDA_RESTAPI_URL) {
-            fetch(url)
-                .then(result => result.json())
+        startServer();
+        fetchCalc()
+            .then(result => {
+                setData(result);
+                setLoaded(true);
+            })
+            .catch(error => setLoaded(false));
+        setInterval(() => {
+            fetchCalc()
                 .then(result => {
                     setData(result);
                 })
-                .catch(error => {
-                    setLoaded(isLoaded);
-                });
-        }
-    }
-        // var data = state.data;
-        return (
-            <React.Fragment>
-            <Paper style={{ minHeight: 300, width: "100%" }}>
-                {data && data.data && <NodesTable data={data} />}
+                .catch(error => setLoaded(true));
+        }, 15000);
+    }, []);
+
+    // console.log(isLoaded);
+    const classes = useStyles();
+    return (
+        <React.Fragment>
+            <Paper className={classes.root}>
+                {isLoaded && data.data && <NodesTable data={data} />}
             </Paper>
-            </React.Fragment>
-        );
+        </React.Fragment>
+    );
 }
